12ec25372588bc3ef046d56299e9a639652b266f
Manohar  Kulat <manoharkulat015@gmail.com>
2021-09-15 22:45:37 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='its a waper project using mlops',
+    author='manoharkulat',
+    license='MIT',
+)
